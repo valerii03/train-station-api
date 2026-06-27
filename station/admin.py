5255e4a6d308b2eb1c0726758aea2106843a06a9
Valerii <valeraivasik602@gmail.com>
2026-06-27 22:19:37 +0100

    Add train models

--- station/admin.py
+++ station/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 
-from station.models import Station
+from station.models import Station, Route, TrainType, Train
 
 
 @admin.register(Station)
 class StationAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "latitude", "longitude")
     search_fields = ("name",)
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("id", "source", "destination", "distance")
+
+
+@admin.register(TrainType)
+class TrainTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "cargo_num",
+        "places_in_cargo",
+        "train_type",
+    )
--- station/models.py
+++ station/models.py
@@ -25,3 +25,24 @@ class Route(models.Model):
 
     def __str__(self):
         return f"{self.source} -> {self.destination}"
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.PositiveIntegerField()
+    places_in_cargo = models.PositiveIntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        on_delete=models.CASCADE,
+        related_name="trains",
+    )
+
+    def __str__(self):
+        return self.name
