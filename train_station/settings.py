49837d4d64ddfdf61336fd25116b4b4ec0240850
Valerii <valeraivasik602@gmail.com>
2026-06-29 22:39:55 +0100

    Add pagination and API documentation

--- train_station/settings.py
+++ train_station/settings.py
@@ -106,7 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = dict(DEFAULT_SCHEMA_CLASS="drf_spectacular.openapi.AutoSchema", DEFAULT_AUTHENTICATION_CLASSES=(
+    "rest_framework_simplejwt.authentication.JWTAuthentication",
+), DEFAULT_PERMISSION_CLASSES=(
+    "rest_framework.permissions.IsAuthenticated",
+), DEFAULT_PAGINATION_CLASS=(
+    "rest_framework.pagination.PageNumberPagination"
+), PAGE_SIZE=10)
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
--- train_station/urls.py
+++ train_station/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/station/", include("station.urls", namespace="station")),
     path("api/user/", include("accounts.urls", namespace="accounts")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
